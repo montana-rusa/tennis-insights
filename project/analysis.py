8bd119cfe53c392c8ba0ae03b60e27e5ac127725
Emma <emlcliffe@gmail.com>
2025-10-26 19:14:48 +0000

    major configure, set up serving stats and removed other features

--- project/analysis.py
+++ project/analysis.py
@@ -9,64 +9,62 @@ pio.renderers.default = "browser"
 allyears = pd.concat(pd.read_csv(f'wta_matches_{year}.csv')
                      .assign(year=year) for year in range(2020,2025))
 
-def calculate_games(score):
+def get_unique_players(matches):
+    unique_players = pd.concat([matches['winner_name'],matches['loser_name']]).unique()
+    return unique_players
 
-    sets = re.findall(r'(\d+)-(\d+)',score)
-    sets = [(int(w),int(l)) for w, l in sets]
+def build_serve_stats_graph(matches = allyears):
+
+    unique_players = pd.concat([matches['winner_name'],matches['loser_name']]).unique()
+    serve = pd.DataFrame(columns=['name','a_per_match','df_per_match', 'fs_per_match', 'ss_per_match'])
+
+    for player in unique_players:
+
+        all_games_played = pd.concat([matches[matches.winner_name==player],matches[matches.loser_name==player]])
+        if len(all_games_played) >= 50:
+
+            count1 = matches[matches.winner_name==player]['w_ace'].sum()
+            count2 = matches[matches.loser_name==player]['l_ace'].sum()
+            a_per_match = (count1 + count2) / len(all_games_played)
+            #aces
+
+            count1 = matches[matches.winner_name==player]['w_1stWon'].sum()
+            count2 = matches[matches.loser_name==player]['l_1stWon'].sum()
+            fs_per_match = (count1 + count2) / len(all_games_played)
+            #first serves
+
+            count1 = matches[matches.winner_name==player]['w_2ndWon'].sum()
+            count2 = matches[matches.loser_name==player]['l_2ndWon'].sum()
+            ss_per_match = (count1 + count2) / len(all_games_played)
+            #second serves
+
+            count1 = matches[matches.winner_name==player]['w_df'].sum()
+            count2 = matches[matches.loser_name==player]['l_df'].sum()
+            df_per_match = (count1 + count2) / len(all_games_played)
+            #second serves
+
+            serve.loc[len(serve)] = [player, a_per_match,df_per_match, fs_per_match, ss_per_match]
     
-    w_games = 0
-    l_games = 0
+    for h in ['a_per_match','df_per_match', 'fs_per_match', 'ss_per_match']:
+        max = serve[h].max()
+        serve[h] = serve[h] / max
 
-    for w, l in sets:
-        w_games += w
-        l_games += l
+    serve['df_per_match'] = 1 - serve['df_per_match']
+    serve['serve_score'] = serve.drop('name',axis=1).sum(axis=1)
+    serve['serve_score'] = serve['serve_score'] / 4
 
-    return w_games, l_games
+    sorted_serves = serve.sort_values(by='serve_score', ascending=False).reset_index(drop=True)
+    sorted_serves.index.name = 'rank'
 
-def get_won_games(name, matches):
-    matches_won = matches[matches.winner_name==name]
-    count = sum(calculate_games(score)[0] for _,score in matches_won.score.items())
-    return count
-   
-def get_lost_games(name, matches):
-    matches_lost = matches[matches.loser_name==name]
-    count = sum(calculate_games(score)[1] for _,score in matches_lost.score.items())
-    return count
 
-def calc_average_games(name,matches):
-
-    games1 = get_won_games(name, matches)
-    games2 = get_lost_games(name, matches)
-    all_games_played = pd.concat([matches[matches.winner_name==name],matches[matches.loser_name==name]])
-    game_count = len(pd.concat([matches[matches.winner_name==name],matches[matches.loser_name==name]]))
-    return (games1 + games2)/game_count
-
-def average_game_length(name, matches):
-    all_matches = pd.concat([matches[matches.winner_name==name], matches[matches.loser_name==name]])
-    average = all_matches.minutes.sum() / len(all_matches)
-    return average
-
-def build_graph(name, metric_name, matches=allyears):
-    ys = []
-    for year in range(2020,2025):
-        matches = pd.read_csv(f'wta_matches_{year}.csv')
-
-        if metric_name == "average_games_per_match":
-            ys.append(calc_average_games(name,matches))
-            data = {
-            'years': [2020,2021,2022,2023,2024],
-            metric_name : ys }
-
-        
-        if metric_name == "average_game_length":
-            ys.append(average_game_length(name,matches))
-            data = {
-            'years': [2020,2021,2022,2023,2024],
-            metric_name : ys }
-    df = pd.DataFrame(data)
-
-    fig = px.line(df, y=metric_name, x='years')
+    fig = px.scatter(
+        sorted_serves,
+        x=sorted_serves.index,
+        y='serve_score',
+        text='name', 
+        title='WTA Servers ranked'
+    )
+    fig.update_traces(textposition='top center')
     return fig
-#average games per match
 
-#build_graph("Iga Swiatek", "average_game_length", allyears)
+build_serve_stats_graph(matches = allyears)
--- project/app.py
+++ project/app.py
@@ -2,16 +2,26 @@ from flask import Flask, request, render_template
 import plotly.io as pio
 import analysis
 
+#requests allows access to HTTP form inputs, 
+#render_template allows generation of HTML content with dynamic content
+#plotly.io allows rendering of plotly graphs with dynamic content
 
 app = Flask(__name__)
+#creates a flask app instance
 
 @app.route('/', methods=['GET','POST'])
+#defining route (home() will handle requests to the route directory)
+#home() can handle both GET requests (like opening the page) and POST requests (like submitting a form)
+
 def home():
 
-    player = request.form.get('fname', 'Iga Swiatek')
-    fig = analysis.build_graph(player, "average_game_length")
-    graph_html = pio.to_html(fig, full_html=False)
-    return render_template('home.html',graph_html=graph_html)
+    stat_type = request.form.get('types', 'serving')
+    #key and default
+
+    if stat_type == 'serving':
+        fig = analysis.build_serve_stats_graph()
+        graph_html = pio.to_html(fig, full_html=False)
+        return render_template('home.html',graph_html=graph_html)
 
 if __name__ == '__main__':
     app.run(debug=True)
